04275ce4caba32e92e6f15b9c7607dba9d097e9c
Daisy Catalan <dscatalan@gmail.com>
2022-09-23 17:35:49 -0700

    first commit

---
+++ app/src/main/java/com/bignerdranch/android/live_demo_constraint_layout/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.live_demo_constraint_layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
